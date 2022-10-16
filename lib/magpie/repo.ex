121793633409a5ae70979779d04231cc3952baa9
Xiang Ji <hi@xiangji.me>
2022-10-16 10:26:00 +0200

    Fix normalize_one_result error message

--- lib/magpie/repo.ex
+++ lib/magpie/repo.ex
@@ -8,9 +8,9 @@ defmodule Magpie.Repo do
   def normalize_one_result(result), do: {:ok, result}
 
   @spec normalize_one_result(any(), String.t()) ::
-          {:ok, any()} | {:error, {:not_found, String.t()}}
+          {:ok, any()} | {:error, String.t()}
   def normalize_one_result(nil, not_found_message) when is_binary(not_found_message),
-    do: {:error, {:not_found, not_found_message}}
+    do: {:error, not_found_message}
 
   def normalize_one_result(result, not_found_message) when is_binary(not_found_message),
     do: {:ok, result}
