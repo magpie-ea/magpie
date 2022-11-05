d650eec634bd0bba3aa96d406fb78c46113239ca
Xiang Ji <hi@xiangji.me>
2022-11-05 09:54:38 +0100

    Remove unused tests

--- test/magpie_web/channels/participant_channel_test.exs
+++ test/magpie_web/channels/participant_channel_test.exs
@@ -5,16 +5,6 @@ defmodule MagpieWeb.ParticipantChannelTest do
     create_participant_and_take_slot()
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push(socket, "ping", %{"hello" => "there"})
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
-
-  test "shout broadcasts to participant:lobby", %{socket: socket} do
-    push(socket, "shout", %{"hello" => "all"})
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
-
   test "broadcasts are pushed to the client", %{socket: socket} do
     broadcast_from!(socket, "broadcast", %{"some" => "data"})
     assert_push "broadcast", %{"some" => "data"}
