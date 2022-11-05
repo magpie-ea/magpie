7597bef5aecbd9b484506feefcd736d1df033181
Xiang Ji <hi@xiangji.me>
2022-11-05 10:16:12 +0100

    Add test for presence_dif

--- test/magpie_web/channels/interactive_room_channel_test.exs
+++ test/magpie_web/channels/interactive_room_channel_test.exs
@@ -21,29 +21,20 @@ defmodule MagpieWeb.InteractiveRoomChannelTest do
              )
   end
 
-  # test "the newly joined user is tracked by Presence", %{
-  #   socket: socket,
-  #   experiment: _experiment,
-  #   participant_id: _participant_id,
-  #   assignment_identifier: assignment_identifier
-  # } do
-  #   {:ok, _, _socket} =
-  #     subscribe_and_join(
-  #       socket,
-  #       "interactive_room:#{assignment_identifier.experiment_id}:#{assignment_identifier.variant}:#{assignment_identifier.chain}:#{assignment_identifier.generation}"
-  #     )
+  test "the newly joined user is tracked by Presence", %{
+    socket: socket
+  } do
+    [_copy, identifier, _player] = String.split(socket.assigns.slot_identifier, "_")
 
-  #   # payload = %{
-  #   #   variant: socket.assigns.variant,
-  #   #   chain: socket.assigns.chain,
-  #   #   generation: socket.assigns.generation
-  #   # }
+    assert {:ok, _, _socket} =
+             subscribe_and_join(
+               socket,
+               "interactive_room:#{identifier}"
+             )
 
-  #   # assert_broadcast("presence_diff", ^payload)
-
-  #   # Just test that it pushed out an event called "presence_diff". It seems to be not that easy to match against the payload body actually.
-  #   assert_broadcast("presence_diff", %{})
-  # end
+    # Just test that it pushed out an event called "presence_diff". It seems to be not that easy to match against the payload body actually.
+    assert_broadcast("presence_diff", %{})
+  end
 
   # test "start_game message is sent after the specified number of participants is reached", %{
   #   socket: socket,
